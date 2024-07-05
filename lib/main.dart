fdb3afc55d88c802ac99b2cc5264817e37176e95
EduarVentura <eduar.ventura@unah.hn>
2024-07-04 23:29:12 -0600

    Update Repository Tarea1.2

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Configuración de Cuenta',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
